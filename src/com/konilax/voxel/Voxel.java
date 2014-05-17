67881b45c8d4e230897d83a96da9353e0f9228a6
Konilax <konilax@gmail.com>
2014-05-16 22:39:15 -0700

    Added Clearing the Screen

--- src/com/konilax/voxel/Voxel.java
+++ src/com/konilax/voxel/Voxel.java
@@ -3,6 +3,10 @@ package com.konilax.voxel;
 import com.nishu.utils.GameLoop;
 import com.nishu.utils.Screen;
 import com.nishu.utils.Window;
+import org.lwjgl.opengl.Display;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.util.glu.GLU.gluPerspective;
 
 public class Voxel extends Screen {
 
@@ -24,7 +28,12 @@ public class Voxel extends Screen {
     }
 
     public void initGL() {
+        glViewport(0, 0, Display.getWidth(), Display.getHeight());
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
 
+        gluPerspective(67.0f, WIDTH / HEIGHT, 0.001f, 1000f);
+        glMatrixMode(GL_MODELVIEW);
     }
 
     public void update() {
@@ -32,7 +41,8 @@ public class Voxel extends Screen {
     }
 
     public void render() {
-
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0, 0, 0.75f, 1);
     }
 
     public void dispose() {
